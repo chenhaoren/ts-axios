d78361fa9735a033c13c71e1b4a4a2c159b331ae
chenguangpeng <chenguangpeng@gtland.cn>
2019-05-07 00:41:21 +0800

    feat(index xhr types/index): request by xhr
    implement a simple function of sending a request by xhr

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Methods =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Methods
+  params: any
+  data: any
+}
---
+++ src/xhr.ts
@@ -0,0 +1,7 @@
+import { AxiosRequestConfig } from './types/index'
+export default function xhr(config: AxiosRequestConfig): void {
+  const { url, method = 'get', data = null } = config
+  const request = new XMLHttpRequest()
+  request.open(method.toUpperCase(), url, true)
+  request.send(data)
+}
