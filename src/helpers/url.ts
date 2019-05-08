b8538e2bde8383d3025ad7a091b249b445053e8a
chenguangpeng <chenguangpeng@gtland.cn>
2019-05-08 23:31:57 +0800

    fix(util url): replace isObject with isPlainObject
    replace isObject with isPlainObject to validate val

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,4 +1,4 @@
-import { isDate, isObject } from './util'
+import { isDate, isPlainObject } from './util'
 
 export function encode(val: string): string {
   return encodeURIComponent(val)
@@ -29,7 +29,7 @@ export function buildUrl(url: string, params: any): string {
     values.forEach(val => {
       if (isDate(val)) {
         val = val.toISOString()
-      } else if (isObject(val)) {
+      } else if (isPlainObject(val)) {
         val = JSON.stringify(val)
       }
 
