d9011c83306a5ecd3f8a6f64ed96837281be5439
chenguangpeng <chenguangpeng@gtland.cn>
2019-05-10 00:22:37 +0800

    fix(xhr data): transform response data
    try to transform response data to a json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import { AxiosResponse } from './types/index'
 import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
 import { buildUrl } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+function transformResponseData(val: AxiosResponse): AxiosResponse {
+  val.data = transformResponse(val.data)
+  return val
 }
 
 function processConfig(config: AxiosRequestConfig): void {
