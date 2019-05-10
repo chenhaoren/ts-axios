1394e21a6470433231dd7de5db43412ef6ccd89b
chenguangpeng <chenguangpeng@gtland.cn>
2019-05-11 00:22:04 +0800

    feat(error.ts xhr.ts): handle error
    handle error by createError function and AxiosError Class

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,4 @@
-import { AxiosResponse } from './types/index'
-import { AxiosRequestConfig, AxiosPromise } from './types'
-import xhr from './xhr'
-import { buildUrl } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function transformResponseData(val: AxiosResponse): AxiosResponse {
-  val.data = transformResponse(val.data)
-  return val
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildUrl(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
+import axios from './axios'
+export * from './types'
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,23 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types/index'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { url, method = 'get', data = null, headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { url, method = 'get', data = null, headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
     request.open(method.toUpperCase(), url, true)
 
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
 
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) return
+      if (request.status === 0) return
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = request.responseType === 'text' ? request.responseText : request.response
       const response: AxiosResponse = {
@@ -23,7 +28,31 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
+    }
+
+    request.onerror = function hadleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
     }
 
     Object.keys(headers).forEach(name => {
